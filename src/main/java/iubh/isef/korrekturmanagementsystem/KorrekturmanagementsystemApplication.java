8bd7deb3df93cc1c81528fa6f133411a6f478701
christiankoeninger <christian.koeninger@iubh-fernstudium.de>
2021-02-23 20:15:37 +0100

    initial commit

---
+++ src/main/java/iubh/isef/korrekturmanagementsystem/KorrekturmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package iubh.isef.korrekturmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorrekturmanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorrekturmanagementsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iubh/isef/korrekturmanagementsystem/KorrekturmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package iubh.isef.korrekturmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorrekturmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
